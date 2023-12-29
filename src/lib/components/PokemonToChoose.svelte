8c9ca2cdebdc244f5073e1ee25571809a4ff85c8
Jocabin <kobyfar@hotmail.com>
2023-12-29 21:43:49 +0100

    edit styles of create char

--- src/lib/components/PokemonToChoose.svelte
+++ src/lib/components/PokemonToChoose.svelte
@@ -16,12 +16,35 @@
 	}
 </script>
 
+<input type="radio" name="pokeid" id="pokemon-{pokemon.id}" value="{pokemon.id}" class="hidden">
+
 <button type="button" on:click={selectPokemon} class="cursor-pointer">
-	<label for="pokemon-{pokemon.id}" class="flex flex-col items-center justify-start gap-2 cursor-pointer border rounded overflow-hidden hover:bg-gray-200">
+	<label for="pokemon-{pokemon.id}"
+				 class="flex flex-col items-center justify-start gap-2 cursor-pointer border rounded overflow-hidden hover:bg-gray-200">
 		<img src="{pokemon.sprites.front_default}" alt="{pokemon.name}">
 		<span class="font-bold capitalize">{pokemon.name}</span>
-		<span class="w-full uppercase font-bold p-2 bg-accent text-white">Choisir ce pokemon</span>
+		<span class="labelBtn w-full uppercase font-bold p-2 bg-accent text-white">
+			<span>Choisir ce pokemon</span>
+			<span class="hidden bg-red-700">Pokémon choisi</span>
+		</span>
 	</label>
 </button>
 
-<input type="radio" name="pokeid" id="pokemon-{pokemon.id}" value="{pokemon.id}" class="hidden">
+<style>
+    input[type=radio]:checked + button > label {
+        background-color: var(--accent);
+        color: white;
+    }
+
+    input[type=radio]:checked + button > label .labelBtn > span:last-of-type {
+        display: block;
+    }
+
+    input[type=radio]:checked + button > label .labelBtn > span:first-of-type {
+        display: none;
+    }
+
+    input[type=radio] + button > label .labelBtn > span:last-of-type {
+        display: none;
+    }
+</style>
--- src/routes/create-char/+page.svelte
+++ src/routes/create-char/+page.svelte
@@ -10,10 +10,13 @@
 			front_default: ''
 		}
 	};
+	$: isPokemonValid = (choosenPokemon.id !== -1) && (choosenPokemon.name !== "")
 </script>
 
-<h1 class="text-2xl font-bold text-center">Choisissez votre pokemon</h1>
-
+<div class="flex flex-col justify-center items-center gap-4">
+	<h1>Choisissez votre pokemon</h1>
+	<a href="/" class="border border-black rounded px-4 py-2 hover:bg-amber-100">Retour à l'accueil</a>
+</div>
 <form method="post" class="p-4 mt-4">
 	<div class="mb-20 pokemonsGrid">
 		{#each pokemons as pokemon (pokemon.id)}
@@ -24,8 +27,8 @@
 	<input type="hidden" name="pokeID" value="{choosenPokemon.id}">
 
 	<div class="fixed left-0 bottom-0 w-full bg-white p-4 border-t flex items-stretch justify-between gap-4">
-		<input type="text" name="pokeName" value="{choosenPokemon.name}" class="border-black border rounded text-2xl p-2 flex-1">
-		<button type="submit" class="bg-accent text-white font-bold uppercase text-3xl p-4 rounded">Créer un combatant</button>
+		<input type="text" name="pokeName" bind:value="{choosenPokemon.name}" class="border-black border rounded text-2xl p-2 flex-1">
+		<button disabled={!isPokemonValid} type="submit" class="bg-accent text-white font-bold uppercase text-3xl p-4 rounded disabled:opacity-20">Créer un combatant</button>
 	</div>
 </form>
 
@@ -35,4 +38,6 @@
 			grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 			gap: 2rem;
 	}
+
+
 </style>
